de55246e013b8ab983468fd9775129a309c5a16b
syntaxErrorLineNil <cyberorange16@gmail.com>
2024-08-30 14:22:02 +0300

    add comment

--- error.go
+++ error.go
@@ -3,7 +3,13 @@ package nats_pubsub_go
 import "errors"
 
 var (
+	// ErrInvalidArgument is an error returned when an invalid argument is provided.
+	// This is used to indicate that a function or method has been called with
+	// arguments that do not meet the required criteria or format.
 	ErrInvalidArgument = errors.New("invalid argument")
 
+	// ErrCloseConnection is an error returned when an operation is attempted
+	// on a closed connection. It signifies that the connection has been
+	// terminated and cannot be used for further operations.
 	ErrCloseConnection = errors.New("connection is close")
 )
