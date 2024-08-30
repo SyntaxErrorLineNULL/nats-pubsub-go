c317878c98969710d2ec83343d78290ce9e33288
syntaxErrorLineNil <cyberorange16@gmail.com>
2024-08-30 15:06:52 +0300

    AsyncSubscribe method

--- pkg/subscriber.go
+++ pkg/subscriber.go
@@ -1,6 +1,9 @@
 package pkg
 
-import "github.com/nats-io/nats.go"
+import (
+	pubsub "github.com/SyntaxErrorLineNULL/nats-pubsub-go"
+	"github.com/nats-io/nats.go"
+)
 
 type Subscriber struct {
 	// conn holds the connection to the NATS server.
@@ -14,4 +17,38 @@ type Subscriber struct {
 
 func NewSubscriber(conn *nats.Conn) *Subscriber {
 	return &Subscriber{conn: conn}
+}
+
+// AsyncSubscribe subscribes to a subject asynchronously and returns a Subscription object
+// that allows receiving messages on the subscribed subject. It handles errors and provides
+// a channel for receiving messages.
+// Docs: https://docs.nats.io/using-nats/developer/receiving/async
+func (s *Subscriber) AsyncSubscribe(subject string) (pubsub.MessageHandler, error) {
+	// Check if the provided subject is empty.
+	// An empty subject is invalid and cannot be subscribed to.
+	// Return an ErrInvalidArgument error to indicate the issue.
+	if len(subject) == 0 {
+		return nil, pubsub.ErrInvalidArgument
+	}
+
+	// Create a channel to receive incoming messages asynchronously.
+	// This channel will be used to pass messages from the NATS subscription callback
+	// to the code that is using the subscription.
+	messages := make(chan *nats.Msg)
+
+	// Subscribe to the specified subject with a callback function that sends
+	// received messages to the messages channel.
+	sub, err := s.conn.Subscribe(subject, func(msg *nats.Msg) { messages <- msg })
+
+	// Check for errors that occurred during subscription.
+	// If an error is returned, it indicates that the subscription could not be created.
+	// Return nil for the Subscription and the error to signal failure.
+	if err != nil {
+		return nil, err
+	}
+
+	// Return a new MessageHandler instance with the created messages channel
+	// and the NATS subscription. This allows the caller to receive and handle
+	// messages asynchronously.
+	return &MessageHandler{Message: messages, Subscription: sub}, nil
 }
