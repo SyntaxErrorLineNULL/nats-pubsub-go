c3a957dd9d87a46011afa5a23134ade5fd9c0660
syntaxErrorLineNil <cyberorange16@gmail.com>
2024-09-26 23:59:41 +0300

    update publisher test cases

--- pkg/publisher_test.go
+++ pkg/publisher_test.go
@@ -13,19 +13,39 @@ import (
 func TestPublisher(t *testing.T) {
 	t.Parallel()
 
-	// Start a mock NATS server for testing purposes
-	err := test.InitNats()
-	// Assert that the NATS server started without errors
-	assert.NoError(t, err, "Expected no error when starting NATS server")
-	// Ensure the NATS server is properly shut down after tests
-	defer test.ShutdownNatsServer()
+	// Initialize the NATS server with a port of 18222.
+	// Passing 0 as the port indicates that the server should choose a default port.
+	// The InitNats function is expected to return an error if there was a failure in starting the NATS server.
+	natsServer := test.NewNatsServer(14222)
+	// Assert that the newly created NatsServer instance is not nil.
+	// This check verifies that the NatsServer was successfully initialized.
+	// If natsServer is nil, the test will fail, indicating an issue with the server creation.
+	assert.NotNil(t, natsServer, "Expected NatsServer to be initialized, but got nil")
 
-	// Get the NATS connection from the global state
-	natsConnection := test.GetNatsConnection()
-	// Assert that the NATS connection is initialized correctly
+	// Initialize the NATS server with a port of 18222.
+	// Passing 0 as the port indicates that the server should choose a default port.
+	// The InitNats function is expected to return an error if there was a failure in starting the NATS server.
+	err := natsServer.InitNats()
+	// Assert that there is no error from the NATS server initialization.
+	// Assert.NoError function checks that `err` (the actual value) is nil.
+	// If `err` is not nil, it means there was an error during the initialization of the NATS server,
+	// and the provided message "Expected no error when starting NATS server" will indicate the nature of the failure.
+	assert.NoError(t, err, "Expected no error when starting NATS server")
+
+	// Schedule the ShutdownNatsServer function to be called when the surrounding function (test) returns.
+	// This ensures that the NATS server will be properly shut down after the test completes,
+	// preventing resource leaks and ensuring a clean state for subsequent tests.
+	defer natsServer.ShutdownNatsServer()
+
+	// Retrieve the NATS connection instance from the test package.
+	// The GetNatsConnection function is expected to return a pointer to an initialized NATS connection object.
+	// This connection will be used for interacting with the NATS messaging system during the tests.
+	natsConnection := natsServer.GetNatsConnection()
+	// Assert that the retrieved NATS connection is not nil.
+	// Assert.NotNil function checks that `natsConnection` (the actual value) is not nil.
+	// If it is nil, the test will fail, and the provided message "Expected the NATS connection to be initialized, but got nil"
+	// will indicate that the connection was not properly initialized before this point in the test.
 	assert.NotNil(t, natsConnection, "Expected the NATS connection to be initialized, but got nil")
-	// Ensure the NATS connection is properly closed after tests
-	// defer natsConnection.Close()
 
 	// Create a new Publisher instance using the provided NATS connection.
 	// This step initializes the Publisher object with the necessary connection
@@ -201,6 +221,42 @@ func TestPublisher(t *testing.T) {
 			t.Fatal("Timed out waiting for message")
 		}
 	})
+}
+
+func TestPublisherClose(t *testing.T) {
+	// Initialize the NATS server with a port of 18222.
+	// Passing 0 as the port indicates that the server should choose a default port.
+	// The InitNats function is expected to return an error if there was a failure in starting the NATS server.
+	natsServer := test.NewNatsServer(24222)
+	// Assert that the newly created NatsServer instance is not nil.
+	// This check verifies that the NatsServer was successfully initialized.
+	// If natsServer is nil, the test will fail, indicating an issue with the server creation.
+	assert.NotNil(t, natsServer, "Expected NatsServer to be initialized, but got nil")
+
+	// Initialize the NATS server with a port of 18222.
+	// Passing 0 as the port indicates that the server should choose a default port.
+	// The InitNats function is expected to return an error if there was a failure in starting the NATS server.
+	err := natsServer.InitNats()
+	// Assert that there is no error from the NATS server initialization.
+	// Assert.NoError function checks that `err` (the actual value) is nil.
+	// If `err` is not nil, it means there was an error during the initialization of the NATS server,
+	// and the provided message "Expected no error when starting NATS server" will indicate the nature of the failure.
+	assert.NoError(t, err, "Expected no error when starting NATS server")
+
+	// Schedule the ShutdownNatsServer function to be called when the surrounding function (test) returns.
+	// This ensures that the NATS server will be properly shut down after the test completes,
+	// preventing resource leaks and ensuring a clean state for subsequent tests.
+	defer natsServer.ShutdownNatsServer()
+
+	// Retrieve the NATS connection instance from the test package.
+	// The GetNatsConnection function is expected to return a pointer to an initialized NATS connection object.
+	// This connection will be used for interacting with the NATS messaging system during the tests.
+	natsConnection := natsServer.GetNatsConnection()
+	// Assert that the retrieved NATS connection is not nil.
+	// Assert.NotNil function checks that `natsConnection` (the actual value) is not nil.
+	// If it is nil, the test will fail, and the provided message "Expected the NATS connection to be initialized, but got nil"
+	// will indicate that the connection was not properly initialized before this point in the test.
+	assert.NotNil(t, natsConnection, "Expected the NATS connection to be initialized, but got nil")
 
 	// Close tests the behavior of the Publisher's Close method. It verifies that the
 	// Close method correctly marks the Publisher as closed and prevents further
@@ -245,19 +301,39 @@ func FuzzPublisher(f *testing.F) {
 	f.Add("test.subject", []byte("test payload"))
 
 	f.Fuzz(func(t *testing.T, subject string, payload []byte) {
-		// Start a mock NATS server for testing purposes
-		err := test.InitNats()
-		// Assert that the NATS server started without errors
-		assert.NoError(t, err, "Expected no error when starting NATS server")
-		// Ensure the NATS server is properly shut down after tests
-		defer test.ShutdownNatsServer()
+		// Initialize the NATS server with a port of 18222.
+		// Passing 0 as the port indicates that the server should choose a default port.
+		// The InitNats function is expected to return an error if there was a failure in starting the NATS server.
+		natsServer := test.NewNatsServer(18222)
+		// Assert that the newly created NatsServer instance is not nil.
+		// This check verifies that the NatsServer was successfully initialized.
+		// If natsServer is nil, the test will fail, indicating an issue with the server creation.
+		assert.NotNil(t, natsServer, "Expected NatsServer to be initialized, but got nil")
 
-		// Get the NATS connection from the global state
-		natsConnection := test.GetNatsConnection()
-		// Assert that the NATS connection is initialized correctly
+		// Initialize the NATS server with a port of 18222.
+		// Passing 0 as the port indicates that the server should choose a default port.
+		// The InitNats function is expected to return an error if there was a failure in starting the NATS server.
+		err := natsServer.InitNats()
+		// Assert that there is no error from the NATS server initialization.
+		// Assert.NoError function checks that `err` (the actual value) is nil.
+		// If `err` is not nil, it means there was an error during the initialization of the NATS server,
+		// and the provided message "Expected no error when starting NATS server" will indicate the nature of the failure.
+		assert.NoError(t, err, "Expected no error when starting NATS server")
+
+		// Schedule the ShutdownNatsServer function to be called when the surrounding function (test) returns.
+		// This ensures that the NATS server will be properly shut down after the test completes,
+		// preventing resource leaks and ensuring a clean state for subsequent tests.
+		defer natsServer.ShutdownNatsServer()
+
+		// Retrieve the NATS connection instance from the test package.
+		// The GetNatsConnection function is expected to return a pointer to an initialized NATS connection object.
+		// This connection will be used for interacting with the NATS messaging system during the tests.
+		natsConnection := natsServer.GetNatsConnection()
+		// Assert that the retrieved NATS connection is not nil.
+		// Assert.NotNil function checks that `natsConnection` (the actual value) is not nil.
+		// If it is nil, the test will fail, and the provided message "Expected the NATS connection to be initialized, but got nil"
+		// will indicate that the connection was not properly initialized before this point in the test.
 		assert.NotNil(t, natsConnection, "Expected the NATS connection to be initialized, but got nil")
-		// Ensure the NATS connection is properly closed after tests
-		// defer natsConnection.Close()
 
 		// Create a new Publisher instance using the provided NATS connection.
 		// This step initializes the Publisher object with the necessary connection
@@ -273,38 +349,87 @@ func FuzzPublisher(f *testing.F) {
 
 		// Edge Case: Check if the subject is empty. NATS subjects should not be empty.
 		if subject == "" {
-			// Expect the Publish to fail with an invalid argument error due to the empty subject.
+			// Attempt to publish a message using an invalid (empty) subject.
+			// The Publish method is expected to return an error because the subject is empty.
+			// This ensures that the system does not allow publishing to an invalid or missing subject.
 			err = publisher.Publish(&nats.Msg{Subject: subject, Data: payload})
+			// Verify that an error occurred when attempting to publish with an empty subject.
+			// This assertion ensures that the system properly detected the invalid input.
 			assert.Error(t, err, "Expected an error due to empty subject")
+			// Check that the specific error returned is `ErrInvalidArgument`.
+			// This ensures that the error handling is consistent and the correct error is reported.
 			assert.ErrorIs(t, err, nats_pubsub_go.ErrInvalidArgument, "Expected ErrInvalidArgument due to empty subject")
+
+			// Return from the test after verifying the error, since the remaining logic
+			// should not proceed with an invalid publishing operation.
 			return
 		}
 
-		// Edge Case: Check for nil payload. Since fuzzing provides a byte slice, it won't be nil, but it could be empty.
-		// We check how the system handles an empty payload.
+		// Edge Case: Check for nil payload.
+		// Fuzzing will not provide a `nil` byte slice because it's a valid data type,
+		// but it could still generate an empty payload (a zero-length byte slice).
+		// We want to ensure that the system handles an empty payload without failure.
 		if len(payload) == 0 {
-			// Empty payload is technically valid in NATS, so ensure no errors occur.
+			// Attempt to publish a message with an empty payload to the NATS server.
+			// The `publisher.Publish` function is responsible for sending the message.
+			// In this case, the message has a valid `subject` but the `Data` field (payload) is empty.
+			// We're testing whether the system gracefully handles this edge case.
 			err = publisher.Publish(&nats.Msg{Subject: subject, Data: payload})
+			// Assert that publishing an empty payload doesn't result in an error.
+			// We're expecting no errors because an empty payload is considered a valid input.
+			// The test ensures that the system allows sending empty messages without raising an error.
+			// The first argument `t` is the test context, and `err` is the result of the publish operation.
+			// If an error occurs, the message in `assert.NoError` will provide a detailed failure description.
 			assert.NoError(t, err, "Expected no error for an empty payload")
+
+			// Since this is an edge case specific to an empty payload, there's no need to proceed with
+			// further assertions or checks. The `return` statement ensures the test case terminates here,
+			// concluding the evaluation of this edge case.
 			return
 		}
 
-		// Subscribe to the subject using the NATS connection, allowing us to receive the message.
+		// Attempt to subscribe to the specified subject synchronously using the NATS connection.
+		// This line invokes the `SubscribeSync` method on the `natsConnection` object.
+		// It registers a subscription for the provided `subject`.
+		// The returned subscription object `sub` will be used for receiving messages,
+		// and `errSub` will capture any errors that occur during the subscription process.
 		sub, errSub := natsConnection.SubscribeSync(subject)
+		// Assert that subscribing to the subject did not return an error.
+		// Assert.NoError function checks that `errSub` is `nil`,
+		// indicating that the subscription was successful.
+		// If an error occurs, the test will fail, and the message
+		// "Failed to subscribe to the subject" will provide context for the failure.
+		// This is crucial because a successful subscription is necessary
+		// for the subsequent message handling to function correctly.
 		assert.NoError(t, errSub, "Failed to subscribe to the subject")
 
-		// Publish the message to the specified subject using the Publisher's Publish method.
+		// Attempt to publish a message to the specified subject using the publisher instance.
+		// This line constructs a new `nats.Msg` object with the provided `subject` and `payload`,
+		// which represents the message data to be sent.
+		// The `Publish` method of the `publisher` sends the message to the NATS server.
 		err = publisher.Publish(&nats.Msg{Subject: subject, Data: payload})
+		// Assert that publishing the message did not return an error.
+		// Assert.NoError function checks that `err` is `nil`,
+		// indicating that the message was successfully published.
+		// If an error occurs during the publishing process, the test will fail,
+		// and the message "Failed to publish message" will provide context for the failure.
+		// This is crucial to ensure that the message delivery system is functioning correctly.
 		assert.NoError(t, err, "Failed to publish message")
 
 		// Retrieve the next message from the subscription with a timeout of 10 milliseconds.
 		msg, errMsg := sub.NextMsg(10 * time.Millisecond)
 
-		// Check if the message was received. Since we're fuzzing, we tolerate cases where messages
-		// might not be received due to invalid inputs; hence, no assert is used here.
+		// Check if errMsg is nil to determine if there were no errors during the message retrieval.
+		// This conditional ensures that the subsequent assertions are only evaluated when there is no error,
+		// indicating that the message has been successfully received and processed.
 		if errMsg == nil {
-			// If a message was received, assert that the subject and payload match the expected values.
+			// Assert that the subject of the received message matches the expected subject.
+			// The assert.Equal function checks that `subject` (the expected value) is equal to `msg.Subject` (the actual value).
+			// If they are not equal, the test will fail, and the message "Expected subject to match" will provide context for the failure.
 			assert.Equal(t, subject, msg.Subject, "Expected subject to match")
+			// Assert that the data payload of the received message matches the expected payload.
+			// Similar to the previous assertion, this checks that `payload` (the expected value) is equal to `msg.Data` (the actual value).
+			// A mismatch will cause the test to fail, indicating that the published message's data was not received correctly.
 			assert.Equal(t, payload, msg.Data, "Expected payload to match")
 		}
 	})
